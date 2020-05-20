341cf8fba1088e03eebbb069da510fcabc9fa15b
Gil Lightnight <kosykh.igor11@gmail.com>
2020-05-20 18:06:56 +0700

    Make map display in window

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <SFML/Graphics.hpp>
+#include "maps.h"
+
+using namespace sf;
+
+class PLAYER {
+
+public:
+  float dx, dy;
+  FloatRect rect;
+  Sprite sprite;
+  float currentFrame;
+
+  PLAYER(Texture &image) {
+    sprite.setTexture(image);
+    rect = FloatRect(50, 40, 29, 29);
+
+    dx = dy = 0.1;
+    currentFrame = 0;
+  }
+};
+
+int main()
+{
+    RenderWindow window( VideoMode(600,600), "Test");
+    
+    Texture t;
+    t.loadFromFile("hero.png");
+
+    PLAYER p(t);
+    
+    CircleShape MyCircle(100.f);
+    MyCircle.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+	}
+
+	    window.clear();
+	    window.clear(Color::White);
+
+	  RectangleShape rectangle(Vector2f(32, 32));
+    for (int i = 0; i < H; i++)
+      for (int j = 0; j < W; j++) {
+        if (TileMap[i][j] == 'W')
+          rectangle.setFillColor(Color::Black);
+        if (TileMap[i][j] == ' ')
+          continue;
+
+        rectangle.setPosition(j * 32 - offsetX, i * 32 - offsetY);
+	window.draw(rectangle);
+      }
+        window.draw(p.sprite);
+        window.display();
+    }
+    
+    return 0;
+}
