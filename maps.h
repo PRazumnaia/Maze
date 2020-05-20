ef1a3c2d4e87ae5f5092509c44e665106c1b7c77
Gil Lightnight <kosykh.igor11@gmail.com>
2020-05-20 17:59:45 +0700

    Make maps.h with first test map

---
+++ maps.h
@@ -0,0 +1,31 @@
+#ifndef MAPPING_H
+#define MAPPING_H
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+float offsetX = 0, offsetY = 0;
+
+const int H = 12;
+const int W = 40;
+
+
+String TileMap[H] = {
+
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W          W                      W    W",
+    "W          W                           W",
+    "WWWWWWW    W                           W",
+    "W          WWWWWWWW    WWWW            W",
+    "W          W           W  W            W",
+    "W   WWWWWWWW           W  W            W",
+    "W                      WWWW            W",
+    "W                                      W",
+    "W          WWWWWW                      W",
+    "W          W                           W",
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWwWW",
+
+};
+
+
+#endif
