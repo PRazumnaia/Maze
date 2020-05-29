9c38e8962bd3555d024af3266c03367387e3cd24
Mihail Denisov <denisov.mihail2013@yandex.ru>
2020-05-29 15:39:39 +0700

    Fix menu

--- main.cpp
+++ main.cpp
@@ -25,35 +25,34 @@ public:
 };
 
 void menu(RenderWindow & window) {
-	Texture menuTexture1, menuTexture3;
+
+	Texture menuTexture1,menuTexture2;
 	menuTexture1.loadFromFile("1.jpg");
-	menuTexture3.loadFromFile("3.jpg");
-	Sprite menu1(menuTexture1), menu3(menuTexture3);
+	menuTexture2.loadFromFile("2.jpg");
+	Sprite menu1(menuTexture1),menu2(menuTexture2);
 	bool isMenu = 1;
 	int menuNum = 0;
 	menu1.setPosition(780, 300);
-	menu3.setPosition(850, 450);
-
+	menu2.setPosition(850, 450);
+ 
 	while (isMenu)
 	{
 		menu1.setColor(Color::White);
-		menu3.setColor(Color::White);
+		menu2.setColor(Color::White);
 		menuNum = 0;
 		window.clear(Color(129, 181, 221));
  
 		if (IntRect(780, 300, 377, 90).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
-		if (IntRect(850, 450, 217, 80).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+		if (IntRect(850, 450, 217, 80).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
  
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
-			if (menuNum == 1) isMenu = false;//если нажали первую кнопку, то выходим из меню 
-			if (menuNum == 3)  { window.close(); isMenu = false; }
- 
+			if (menuNum == 1) isMenu = false;
+			if (menuNum == 2)  { window.close(); isMenu = false; }
 		}
  
 		window.draw(menu1);
-		window.draw(menu3);
-		
+		window.draw(menu2);		
 		window.display();
 	}
 }
