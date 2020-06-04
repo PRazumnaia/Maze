a45bafb8111e77d6ff29d98b3c258a4b83e352fd
PRazumnaia <polina.razumnaia@gmail.com>
2020-06-04 16:00:45 +0700

    Add update function

---
+++ score.cpp
@@ -0,0 +1,8 @@
+#include "score.h"
+
+int score(int score)
+{
+    int count = 0;
+    count += score;
+    return count;
+}
---
+++ score.h
@@ -0,0 +1,6 @@
+#ifndef score_H
+#define score_H
+
+int score(int score)
+
+#endif
