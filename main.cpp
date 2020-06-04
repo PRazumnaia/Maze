d628e710bab23f244033c5a3bb0bf0bf3aa0a6da
PRazumnaia <polina.razumnaia@gmail.com>
2020-06-04 16:03:24 +0700

    Add collision function

--- main.cpp
+++ main.cpp
@@ -31,6 +31,34 @@ public:
             dx = 0;
             dy = 0;
     }
+
+    void Collision(int dir)
+    {
+
+      for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
+        for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++)
+        {
+          if (TileMap[i][j] == 'B')
+          {
+            if ((dx > 0) && (dir == 0))
+              rect.left = j * 32 - rect.width;
+            if ((dx < 0) && (dir == 0))
+              rect.left = j * 32 + 32;
+            if ((dy > 0) && (dir == 1))
+            {
+              rect.top = i * 32 - rect.height;
+              dy = 0;
+            }
+            if ((dy < 0) && (dir == 1))
+            {
+              rect.top = i * 32 + 32;
+              dy = 0;
+            }
+        }
+       }
+    }
+
+
 };
 
 void menu(RenderWindow & window) {
